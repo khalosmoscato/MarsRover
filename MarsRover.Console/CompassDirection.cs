2ccb53b3e9ce6893d428be5e1d59dee9e258bd97
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-04-13 12:04:05 +0100

    feat: Add Instruction and CompassDirection enums

---
+++ MarsRover.Console/CompassDirection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.Console;
+
+internal enum CompassDirection
+{
+    North,
+    East,
+    South,
+    West
+}
---
+++ MarsRover.Console/Instruction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarsRover.Console;
+
+internal enum Instruction
+{
+    L,
+    R,
+    M
+}
