7479210a72482e130af40da63fcd28b4852b0814
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-04-13 22:12:04 +0100

    feat: Add InstructionParser and tests

--- MarsRover.Console/Parsers/InstructionParser.cs
+++ MarsRover.Console/Parsers/InstructionParser.cs
@@ -1,9 +1,23 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace MarsRover.Console.Parsers;
+using MarsRover.Console.Models;
 
-//internal class InstructionParser
-//{
-//}
+namespace MarsRover.Console.Parsers;
+
+public class InstructionParser
+{
+    public static List<Instruction> ParseInstruction(string s)
+    {
+        if (s == null) throw new ArgumentNullException(nameof(s), "Input cannot be null");
+        var normalisedString = s.ToUpper().Replace(" ", "");
+        return normalisedString.Select(c => c switch
+            {
+                'R' => Instruction.R,
+                'M' => Instruction.M,
+                'L' => Instruction.L,
+                _ => throw new ArgumentException($"Invalid instruction: {s}")
+            }).ToList();
+    }
+}
--- MarsRover.Tests/Parser/InstructionParserTests.cs
+++ MarsRover.Tests/Parser/InstructionParserTests.cs
@@ -1,18 +1,86 @@
-﻿//using MarsRover.Console.Models;
-//using MarsRover.Console.Parsers;
+﻿using MarsRover.Console.Models;
+using MarsRover.Console.Parsers;
 
-//namespace MarsRover.Tests;
+namespace MarsRover.Tests;
 
-//public class InstructionParserTests
-//{
-//    [SetUp]
-//    public void Setup()
-//    {
-//    }
+public class InstructionParserTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+    [Test]
+    public void ParseInstruction_ShouldReturnEmptyList_WhenPassedWhiteSpace()
+    {
+        string input = "";
+        var result = InstructionParser.ParseInstruction(input);
+        var expected = new List<Instruction>();
+        Assert.That(result, Is.EqualTo(expected));
+    }
 
-//    [Test]
-//    public void ParseInstruction_ShouldReturn()
-//    {
-//        Assert.Pass();
-//    }
-//}
+    [Test]
+    public void ParseInstruction_ShouldThrowArgumentNullException_WhenInputIsNull()
+    {
+        Assert.Throws<ArgumentNullException>(() =>
+        InstructionParser.ParseInstruction(null!)
+        );
+    }
+
+    [Test]
+    public void ParseInstruction_ShouldReturnListOfInstructions_WhenGivenValidInput()
+    {
+        string input = "RML";
+
+        var expected = new List<Instruction>
+        {
+            Instruction.R,
+            Instruction.M,
+            Instruction.L
+        };
+
+        var result = InstructionParser.ParseInstruction(input);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [TestCase("LMR", 3)]
+    [TestCase("MMMMM", 5)]
+    [TestCase("", 0)] 
+    public void ParseInstruction_ShouldReturnCorrectCount_WhenValidInputProvided(string input, int expectedCount)
+    {
+        var result = InstructionParser.ParseInstruction(input);
+        Assert.That(result.Count, Is.EqualTo(expectedCount));
+    }
+
+    [Test]
+    public void ParseInstruction_ShouldMaintainCorrectOrder()
+    {
+        var result = InstructionParser.ParseInstruction("MRL");
+
+        Assert.Multiple(() => {
+            Assert.That(result[0], Is.EqualTo(Instruction.M));
+            Assert.That(result[1], Is.EqualTo(Instruction.R));
+            Assert.That(result[2], Is.EqualTo(Instruction.L));
+        });
+    }
+
+    [TestCase("l", Instruction.L)]
+    [TestCase("m", Instruction.M)]
+    [TestCase("r", Instruction.R)]
+    public void ParseInstruction_ShouldHandleLowerCaseInput(string input, Instruction expected)
+    {
+        var result = InstructionParser.ParseInstruction(input);
+        Assert.That(result[0], Is.EqualTo(expected), $"Lower cases such as '{input}' are handled correctly");
+    }
+
+    [Test]
+    public void ParseInstruction_ShouldIgnoreWhiteSpaces_WhenGivenStringWithAMixOfCharsAndWhiteSpaces()
+    {
+        var result = InstructionParser.ParseInstruction("R R L");
+        Assert.Multiple(() => {
+            Assert.That(result[0], Is.EqualTo(Instruction.R));
+            Assert.That(result[1], Is.EqualTo(Instruction.R));
+            Assert.That(result[2], Is.EqualTo(Instruction.L));
+        });
+    }
+}
