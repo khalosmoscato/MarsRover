ad7eb66ea5c729351454b6ba507d9483399553cc
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-04-13 15:33:29 +0100

    feat: Add Parser folder and DirectionParser file and respective folder and file for testing

---
+++ MarsRover.Tests/Parser/DirectionParserTests.cs
@@ -0,0 +1,31 @@
+﻿using MarsRover.Console.Models;
+using MarsRover.Console.Parsers;
+
+namespace MarsRover.Tests;
+
+public class DirectionParserTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestCase('N', CompassDirection.North)]
+    [TestCase('E', CompassDirection.East)]
+    [TestCase('S', CompassDirection.South)]
+    [TestCase('W', CompassDirection.West)]
+    public void ParseDirection_ShouldReturnCorrectDirection_WhenGivenValidChar(char input, CompassDirection expected)
+    {
+        var result = DirectionParser.ParseDirection(input);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void ParseDirection_ShouldThrowAnError_WhenGivenInvalidInput()
+    {
+        var invalidInput = 'A';
+
+        Assert.Throws<ArgumentException>(() =>
+        DirectionParser.ParseDirection(invalidInput), "The parser should throw an ArgumentException when an invalid input is provided");
+    }
+}
---
+++ MarsRover.Tests/Parser/InstructionParserTests.cs
@@ -0,0 +1,18 @@
+﻿//using MarsRover.Console.Models;
+//using MarsRover.Console.Parsers;
+
+//namespace MarsRover.Tests;
+
+//public class InstructionParserTests
+//{
+//    [SetUp]
+//    public void Setup()
+//    {
+//    }
+
+//    [Test]
+//    public void ParseInstruction_ShouldReturn()
+//    {
+//        Assert.Pass();
+//    }
+//}
