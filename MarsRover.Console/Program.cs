41fc4c45d1af8b984d8fc9c412d1d385e50985ff
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-04-16 15:06:34 +0100

    feat: implement basic orchestration in Program.cs to parse example input

--- MarsRover.Console/Program.cs
+++ MarsRover.Console/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using MarsRover.Console.Models;
+using MarsRover.Console.Parsers;
+
+string plateau1 = "5 5";
+string roverPosition = "1 2 N";
+
+Console.WriteLine("--- Mars Rover Orchestrator Starting ---");
+
+var plateauSize = PlateauSizeParser.ParsePlateau(plateau1);
+var initialRoverPosition = PositionParser.ParsePosition(roverPosition);
+
+Console.WriteLine(plateauSize);
+Console.WriteLine(initialRoverPosition);
